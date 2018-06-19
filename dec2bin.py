b290eb9f817f4321bdfb8b877b6284bc09bbfe5d
jeffddjt <04155722@163.com>
2018-06-20 07:56:55 +0800

    Change the prompt to chinese

--- dec2bin.py
+++ dec2bin.py
@@ -12,7 +12,7 @@ def dec2bin(string_num):
         if num == 0: #判断数值是否为0
             return ''.join(la[::-1]) #如果为0，说明已经除完，则将数组翻转并拼接成字符串输出；否则进行下一轮循环
 
-type = sys.getfilesystemencoding()
-dec = input('Input the numeric value to be converted:') #等待用户从控制台输入数值
+
+dec = input('输入待转换的数值:') #等待用户从控制台输入数值
 bin = dec2bin(dec)  #调用函数将输入的十进制数据转换为二进制数据
-print('The number is converted to binary:', bin) #显示提示信息及转换结果
+print('该数值转换为二进制是:', bin) #显示提示信息及转换结果
